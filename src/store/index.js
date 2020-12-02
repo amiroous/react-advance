59f79ef44ed668643c34c0287ed1b490a976ab86
Amir M <36576616+amiroous@users.noreply.github.com>
2020-12-02 08:43:54 -0800

    Update index.js

--- src/store/index.js
+++ src/store/index.js
@@ -16,3 +16,55 @@ const StoreProvider = ({children}) => {
 export const useStateData = () => useContext(store);
 
 export default StoreProvider;
+
+
+/*
+import React, { createContext, useContext, useReducer } from 'react';
+
+const SET_ACTIVE_ITEM = 'SET_ACTIVE_ITEM';
+
+const initialState = {
+  activeItem: null
+};
+
+const appContext = createContext(initialState);
+export const useAppStore = () => useContext(appContext);
+
+const appStoreReducer = (state, action) => {
+  const { type, payload } = action;
+  switch (type) {
+    case SET_ACTIVE_ITEM:
+      return { ...state, activeItem: payload };
+    default:
+      return state;
+  }
+};
+
+export const AppStoreProvider = ({ propsToState, children }) => {
+  const currentState = { ...initialState, ...propsToState };
+  const [state, dispatch] = useReducer(appStoreReducer, currentState, undefined);
+  return (
+    <appContext.Provider value={[state, dispatch]}>
+      {children}
+    </appContext.Provider>
+  )
+};
+
+/***
+ * Usage
+ */
+/*
+// Wrap Top Component in the Provider
+<AppStoreProvider propsToState={{ activeItem: 'ITEM_B' }}>{children}</AppStoreProvider>
+
+// Use Store (Read/Write) Wherever Needed
+const [{activeItem}, dispatch] = useAppStore();
+dispatch({
+  type: SET_ACTIVE_ITEM,
+  payload: {
+    activeItem: 'ITEM_A'
+  }
+});
+*/
+
+*/
